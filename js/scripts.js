42f2acd0d537ee198675bc5ef53ee0decdb30307
Setsuya <tiagomscabral@gmail.com>
2017-11-15 23:30:49 -0200

    Included image and code to show a banner and info when sharing the tool on other platforms.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ $(document).ready(function(){
 		$(this).html(langs[lang][$(this).attr("id")]);
 	});
 
+	$("meta[property='og:description']").attr("content", langs[lang].string_share);
+
 	original_button_position = $("#reload_buttons").offset().top;
 });
 
@@ -45,58 +47,60 @@ var factions_xp = {
 var langs = {
 	"en-US": {
 		  "string_characters": "Characters", 
-		     "string_rewards": "Clan Rewards", 
+			 "string_rewards": "Clan Rewards", 
 			 "string_clan_xp": "Clan XP", 
 		  "string_milestones": "Milestones", 
-		    "string_factions": "Factions", 
-		     "string_loading": "Loading...", 
-		      "string_hunter": "Hunter", 
-		       "string_titan": "Titan", 
-		     "string_warlock": "Warlock", 
-		       "string_level": "Level", 
-		        "string_male": "Male", 
-		      "string_female": "Female", 
-		         "string_exo": "Exo", 
-		       "string_human": "Human", 
-		      "string_awoken": "Awoken", 
+			"string_factions": "Factions", 
+			 "string_loading": "Loading...", 
+			  "string_hunter": "Hunter", 
+			   "string_titan": "Titan", 
+			 "string_warlock": "Warlock", 
+			   "string_level": "Level", 
+				"string_male": "Male", 
+			  "string_female": "Female", 
+				 "string_exo": "Exo", 
+			   "string_human": "Human", 
+			  "string_awoken": "Awoken", 
 		  "string_total_time": "Total play time", 
-		     "string_refresh": "Refresh", 
+			 "string_refresh": "Refresh", 
 		"string_auto_refresh": "Auto", 
 		"string_refresh_time": "(Refreshing automatically every 5 minutes.)", 
-		      "string_legend": "Legend", 
+			  "string_legend": "Legend", 
 		 "string_legend_desc": "Your ongoing deeds add to your legend. Progress to your next Bright Engram.", 
-		       "string_error": "User not found or error occurred.<br />Please try again.", 
-		     "string_warning": "<p>Destiny Faction Checker needs access to your inventory to be able to show faction experience from tokens and materials in your possession. You can still use this tool, only it won't show when you have enough materials for a rank up with the factions. In order to make this information publicly available you need to follow some simple steps.</p><p>Go to <b>Bungie.net</b> and log into your account. Then go to <b>Settings</b> &gt; <b>Privacy</b> and check the option that says &quot;<b>Show my non-equipped Inventory</b>&quot;.</p><p>After this is done, enter your information again and enjoy.</p>", 
-		      "string_donate": "PayPal me some spicy ramen!", 
-			  "string_github": "Check me out on"
+			   "string_error": "User not found or error occurred.<br />Please try again.", 
+			 "string_warning": "<p>Destiny Faction Checker needs access to your inventory to be able to show faction experience from tokens and materials in your possession. You can still use this tool, only it won't show when you have enough materials for a rank up with the factions. In order to make this information publicly available you need to follow some simple steps.</p><p>Go to <b>Bungie.net</b> and log into your account. Then go to <b>Settings</b> &gt; <b>Privacy</b> and check the option that says &quot;<b>Show my non-equipped Inventory</b>&quot;.</p><p>After this is done, enter your information again and enjoy.</p>", 
+			  "string_donate": "PayPal me some spicy ramen!", 
+			  "string_github": "Check me out on", 
+			   "string_share": "Keep track of your characters' progress with your clan, milestones and factions in Destiny 2."
 		
 	}, 
 	"pt-BR": {
 		  "string_characters": "Personagens", 
-		     "string_rewards": "Prêmios de Clã", 
+			 "string_rewards": "Prêmios de Clã", 
 			 "string_clan_xp": "EXP Clã", 
 		  "string_milestones": "Marcos", 
-		    "string_factions": "Facções", 
-		     "string_loading": "Carregando...", 
-		      "string_hunter": "Caçador", 
-		       "string_titan": "Titã", 
-		     "string_warlock": "Arcano", 
-		       "string_level": "Nível", 
-		        "string_male": "Macho", 
-		      "string_female": "Fêmea", 
-		         "string_exo": "Exo", 
-		       "string_human": "Humano", 
-		      "string_awoken": "Desperto", 
+			"string_factions": "Facções", 
+			 "string_loading": "Carregando...", 
+			  "string_hunter": "Caçador", 
+			   "string_titan": "Titã", 
+			 "string_warlock": "Arcano", 
+			   "string_level": "Nível", 
+				"string_male": "Macho", 
+			  "string_female": "Fêmea", 
+				 "string_exo": "Exo", 
+			   "string_human": "Humano", 
+			  "string_awoken": "Desperto", 
 		  "string_total_time": "Tempo de jogo total", 
-		     "string_refresh": "Atualizar", 
-		      "string_legend": "Lenda", 
+			 "string_refresh": "Atualizar", 
+			  "string_legend": "Lenda", 
 		 "string_legend_desc": "Os seus atos de bravura se somam a sua lenda. Progresso até o seu próximo Engrama Brilhante.", 
 		"string_auto_refresh": "Auto", 
 		"string_refresh_time": "(Atualizando automaticamente a cada 5 minutos.)", 
-		       "string_error": "Usuário não encontrado ou erro ocorrido.<br />Por favor tente novamente.", 
-		     "string_warning": "<p>Destiny Faction Checker precisa de acesso ao seu inventário para poder exibir experiência de facções a partir de medalhas e materiais na sua posse. Você ainda pode utilizar esta ferramenta, ela apenas não irá dizer quando você possuir materiais suficientes para subir de nível com as facções. Para deixar essa informação disponível publicamentevocê precisa seguir alguns simples passos.</p><p>Vá até <b>Bungie.net</b> e acesse sua conta. Então vá até <b>Configurações</b> &gt; <b>Privacidade</b> e marque a opção que diz &quot;<b>Mostrar meu Inventário não equipado</b>&quot;.</p><p>Após fazer isso, coloque suas informações novamente e aproveite.</p>", 
-		      "string_donate": "Me pague um ramen apimentado pelo PayPal!", 
-			  "string_github": "Dê uma olhada no meu"
+			   "string_error": "Usuário não encontrado ou erro ocorrido.<br />Por favor tente novamente.", 
+			 "string_warning": "<p>Destiny Faction Checker precisa de acesso ao seu inventário para poder exibir experiência de facções a partir de medalhas e materiais na sua posse. Você ainda pode utilizar esta ferramenta, ela apenas não irá dizer quando você possuir materiais suficientes para subir de nível com as facções. Para deixar essa informação disponível publicamentevocê precisa seguir alguns simples passos.</p><p>Vá até <b>Bungie.net</b> e acesse sua conta. Então vá até <b>Configurações</b> &gt; <b>Privacidade</b> e marque a opção que diz &quot;<b>Mostrar meu Inventário não equipado</b>&quot;.</p><p>Após fazer isso, coloque suas informações novamente e aproveite.</p>", 
+			  "string_donate": "Me pague um ramen apimentado pelo PayPal!", 
+			  "string_github": "Dê uma olhada no meu", 
+			   "string_share": "Acompanhe o progresso dos seus personagens com seu clã, marcos e facções em Destiny 2."
 	}
 };
 
