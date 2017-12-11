046d020107c5f5363fbf8006c050e70b809a30b5
Setsuya <tiagomscabral@gmail.com>
2017-12-11 19:43:35 -0200

    Separated translations and put them in their own file, included a settings screen and allowed the user to change the auto refresh timer and to show numeric values of experience for the factions. Also removed the Facebook button since it was annoying how it could make the page SLOWER to load a simple share button.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,21 @@ $(document).ready(function(){
 		$("#language_flag").attr("src", "img/" + lang + ".png");
 	}
 
+	if(!localStorage.timer_interval){
+		localStorage.timer_interval = "5";
+	}
+
+	if(!localStorage.show_xp_numbers){
+		localStorage.show_xp_numbers = "off";
+	}
+
+	$("input[name=refresh_interval], input[name=show_xp]").removeAttr("checked");
+
+	$("#time_" + localStorage.timer_interval + "min").attr("checked", "checked");
+	timer_time = (parseInt(localStorage.timer_interval) * 60) * 1000;
+
+	$("#xp_" + localStorage.show_xp_numbers).attr("checked", "checked");
+
 	if(localStorage.repeat_timer){
 		repeat_timer = localStorage.repeat_timer === "true";
 	}
@@ -15,7 +30,7 @@ $(document).ready(function(){
 		$("#manifest_ver > span").text(localStorage.curr_ver);
 	}
 
-	$(".section_title > span, #reload_info_off span, #auto_reload_off span, #ramen_donate > p, #string_warning, #string_clan_xp, #string_github, #string_translate").each(function(){
+	$(".section_title > span, #reload_info_off span, #auto_reload_off span, #ramen_donate > p, #string_warning, #string_clan_xp, #string_github, #string_translate, #string_show_xp_numbers, #string_option_on, #string_option_off, #string_show_refresh_interval, #string_apply").each(function(){
 		$(this).html(langs[lang][$(this).attr("id")]);
 	});
 
@@ -56,76 +71,6 @@ var factions_xp = {
 	3398051042: 0, //Dead Orbit
 	1761642340: 0, //Iron Banner
 	2677528157: 0  //Follower of Osiris
-};
-
-var langs = {
-	"en-US": {
-						 "id": "en", 
-		  "string_characters": "Characters", 
-			 "string_rewards": "Clan Rewards", 
-			 "string_clan_xp": "Clan XP", 
-		  "string_milestones": "Milestones", 
-			"string_factions": "Factions", 
-			 "string_loading": "Loading...", 
-			  "string_hunter": "Hunter", 
-			   "string_titan": "Titan", 
-			 "string_warlock": "Warlock", 
-			   "string_level": "Level", 
-				"string_male": "Male", 
-			  "string_female": "Female", 
-				 "string_exo": "Exo", 
-			   "string_human": "Human", 
-			  "string_awoken": "Awoken", 
-		  "string_total_time": "Total play time", 
-			 "string_refresh": "Refresh", 
-		"string_auto_refresh": "Auto", 
-		"string_refresh_time": "(Refreshing automatically every 5 minutes.)", 
-			  "string_legend": "Legend", 
-		 "string_legend_desc": "Your ongoing deeds add to your legend. Progress to your next Bright Engram.", 
-			   "string_error": "User not found or error occurred.<br />Please try again.", 
-			 "string_warning": "<p>Destiny Faction Checker needs access to your inventory to be able to show faction experience from tokens and materials in your possession. You can still use this tool, only it won't show when you have enough materials for a rank up with the factions. In order to make this information publicly available you need to follow some simple steps.</p><p>Go to <b>Bungie.net</b> and log into your account. Then go to <b>Settings</b> &gt; <b>Privacy</b> and check the option that says &quot;<b>Show my non-equipped Inventory</b>&quot;.</p><p>After this is done, enter your information again and enjoy.</p>", 
-			  "string_donate": "PayPal me some spicy ramen!", 
-			  "string_github": "Check me out on", 
-			   "string_share": "Keep track of your characters' progress with your clan, milestones and factions in Destiny 2.", 
-		   "string_translate": "Help me translate this tool into your language!", 
-	   "string_updating_info": "Fetching information, please wait.", 
-	"string_downloading_info": "Downloading:", 
-		 "string_update_done": "DONE!"
-		
-	}, 
-	"pt-BR": {
-						 "id": "pt-br", 
-		  "string_characters": "Personagens", 
-			 "string_rewards": "Prêmios de Clã", 
-			 "string_clan_xp": "EXP Clã", 
-		  "string_milestones": "Marcos", 
-			"string_factions": "Facções", 
-			 "string_loading": "Carregando...", 
-			  "string_hunter": "Caçador", 
-			   "string_titan": "Titã", 
-			 "string_warlock": "Arcano", 
-			   "string_level": "Nível", 
-				"string_male": "Macho", 
-			  "string_female": "Fêmea", 
-				 "string_exo": "Exo", 
-			   "string_human": "Humano", 
-			  "string_awoken": "Desperto", 
-		  "string_total_time": "Tempo de jogo total", 
-			 "string_refresh": "Atualizar", 
-			  "string_legend": "Lenda", 
-		 "string_legend_desc": "Os seus atos de bravura se somam a sua lenda. Progresso até o seu próximo Engrama Brilhante.", 
-		"string_auto_refresh": "Auto", 
-		"string_refresh_time": "(Atualizando automaticamente a cada 5 minutos.)", 
-			   "string_error": "Usuário não encontrado ou erro ocorrido.<br />Por favor tente novamente.", 
-			 "string_warning": "<p>Destiny Faction Checker precisa de acesso ao seu inventário para poder exibir experiência de facções a partir de medalhas e materiais na sua posse. Você ainda pode utilizar esta ferramenta, ela apenas não irá dizer quando você possuir materiais suficientes para subir de nível com as facções. Para deixar essa informação disponível publicamentevocê precisa seguir alguns simples passos.</p><p>Vá até <b>Bungie.net</b> e acesse sua conta. Então vá até <b>Configurações</b> &gt; <b>Privacidade</b> e marque a opção que diz &quot;<b>Mostrar meu Inventário não equipado</b>&quot;.</p><p>Após fazer isso, coloque suas informações novamente e aproveite.</p>", 
-			  "string_donate": "Me pague um ramen apimentado pelo PayPal!", 
-			  "string_github": "Dê uma olhada no meu", 
-			   "string_share": "Acompanhe o progresso dos seus personagens com seu clã, marcos e facções em Destiny 2.", 
-		   "string_translate": "Ajude a traduzir essa ferramenta para o seu idioma!", 
-	   "string_updating_info": "Buscando informações, por favor aguarde.", 
-	"string_downloading_info": "Fazendo download:", 
-		 "string_update_done": "PRONTO!"
-	}
 };
 
 function randomIcon(){
@@ -214,7 +159,7 @@ function toggleAutoReload(toggle_value, membership_type, membership_id, characte
 		$("#auto_reload").attr("class", "repeat_on").attr("onclick", script_text.replace("on", "off"));
 		timer_start_time = new Date().getTime();
 		createTimeout(timer_time);
-		$("#interval_info").text(langs[lang].string_refresh_time).slideToggle("medium");
+		$("#interval_info").text(langs[lang].string_refresh_time.replace("XXX", localStorage.timer_interval)).slideToggle("medium");
 		repeat_timer = true;
 	}
 
@@ -314,6 +259,25 @@ function zeroPad(number, size){
 	return number;
 }
 
+function showOptions(){
+	$("#options").fadeIn("fast", function(){$("#options_contents").slideDown();});
+}
+
+function saveOptions(){
+	localStorage.show_xp_numbers = $("input[name=show_xp]:checked").attr("data-display-xp");
+	localStorage.timer_interval = $("input[name=refresh_interval]:checked").attr("data-interval");
+
+	timer_time = (parseInt(localStorage.timer_interval) * 60) * 1000;
+
+	if(localStorage.show_xp_numbers == "on"){
+		$(".xp_numbers").addClass("numbers_on").removeClass("numbers_off");
+	}else{
+		$(".xp_numbers").addClass("numbers_off").removeClass("numbers_on");
+	}
+
+	$("#options_contents").slideUp(function(){$("#options").fadeOut("fast");});
+}
+
 function checkManifestVersion(){
 	$.ajax({
 		url: "https://www.bungie.net/Platform/Destiny2/Manifest/", 
@@ -338,7 +302,7 @@ function checkManifestVersion(){
 }
 
 function getManifestDBs(urls){
-	$("<div id=\"overlay\"><div id=\"overlay_contents\"><p>" + langs[lang].string_updating_info + "</p><p>" + langs[lang].string_downloading_info + " <span id=\"download_progress\">0%</span></p></div></div>").appendTo("body").fadeIn("slow", function(){$("#overlay_contents").slideDown();});
+	$("<div id=\"overlay\"><div id=\"overlay_contents\"><p>" + langs[lang].string_updating_info + "</p><p>" + langs[lang].string_downloading_info + " <span id=\"download_progress\"></span></p></div></div>").appendTo("body").fadeIn("slow", function(){$("#overlay_contents").slideDown();});
 
 	for(url in urls){
 		url_string = "https://www.bungie.net" + urls[url];
@@ -350,7 +314,7 @@ function getManifestDBs(urls){
 					
 					if(lang_files.length == Object.keys(urls).length){
 						for(i = 0; i < lang_files.length; i++){
-							jszip.file(result.files[Object.keys(result.files)[i]].name).async("uint8array", function showProgress(metadata){$("#download_progress").text(metadata.percent.toFixed(0) + "%");})
+							jszip.file(result.files[Object.keys(result.files)[i]].name).async("uint8array")
 								.then(function(data){
 									getTableData(data);
 								});
@@ -522,7 +486,7 @@ function loadCharacterData(membership_type, membership_id, character_id, charact
 					legend_level = data.Response.progressions.data.progressions["2030054750"].level;
 					legend_percentage = ((data.Response.progressions.data.progressions["2030054750"].progressToNextLevel / data.Response.progressions.data.progressions["2030054750"].nextLevelAt) * 100).toFixed(2);
 
-					factions_result += "<div class=\"faction\"><div class=\"img_col\"><img class=\"faction_img\" src=\"img/factions/bright_engram.png\" /></div><div class=\"info_col\"><p class=\"faction_name\">" + langs[lang].string_legend + "</p><p class=\"faction_description\">" + langs[lang].string_legend_desc + "</p><hr /><div class=\"level_progression\"><div class=\"faction_xp_extra_left\"><div class=\"level_number\">" + (legend_level) + "</div></div><div class=\"faction_xp\"><div class=\"xp_bar\" style=\"width: " + legend_percentage + "%;\"></div></div><div class=\"faction_xp_extra_right\"><div class=\"level_number\">" + (legend_level + 1) + "</div></div></div></div></div>";
+					factions_result += "<div class=\"faction\"><div class=\"img_col\"><img class=\"faction_img\" src=\"img/factions/bright_engram.png\" /></div><div class=\"info_col\"><p class=\"faction_name\">" + langs[lang].string_legend + "</p><p class=\"faction_description\">" + langs[lang].string_legend_desc + "</p><hr /><div class=\"level_progression\"><div class=\"faction_xp_extra_left\"><div class=\"level_number\">" + (legend_level) + "</div></div><div class=\"faction_xp\"><p class=\"xp_numbers numbers_" + localStorage.show_xp_numbers + "\">" + data.Response.progressions.data.progressions["2030054750"].progressToNextLevel + "/" + data.Response.progressions.data.progressions["2030054750"].nextLevelAt + "</p><div class=\"xp_bar\" style=\"width: " + legend_percentage + "%;\"></div></div><div class=\"faction_xp_extra_right\"><div class=\"level_number\">" + (legend_level + 1) + "</div></div></div></div></div>";
 
 					for(faction in factions){
 						$.when(getFactionInfo(faction))
@@ -548,7 +512,7 @@ function loadCharacterData(membership_type, membership_id, character_id, charact
 										outline_class = " faction_outline";
 									}
 
-									factions_result += "<div class=\"faction" + outline_class + "\"><div class=\"img_col\"><img class=\"faction_img\" src=\"img/factions/" + data.id + ".png\" /></div><div class=\"info_col\"><p class=\"faction_name\">" + data.name + "</p><p class=\"faction_description\">" + data.description + "</p><hr /><div class=\"level_progression\"><div class=\"faction_xp_extra_left\"><div class=\"level_number\">" + (factions[data.id].level) + "</div></div><div class=\"faction_xp\"><div class=\"xp_bar\" style=\"width: " + next_level_percentage + "%;\"></div><div class=\"next_xp_bar\" style=\"width: " + remaining_experience + "%;\"></div></div><div class=\"faction_xp_extra_right\"><div class=\"level_number\">" + (factions[data.id].level + 1) + "</div><div class=\"extra_levels\">" + extra_levels + "</div></div></div></div></div>";
+									factions_result += "<div class=\"faction" + outline_class + "\"><div class=\"img_col\"><img class=\"faction_img\" src=\"img/factions/" + data.id + ".png\" /></div><div class=\"info_col\"><p class=\"faction_name\">" + data.name + "</p><p class=\"faction_description\">" + data.description + "</p><hr /><div class=\"level_progression\"><div class=\"faction_xp_extra_left\"><div class=\"level_number\">" + (factions[data.id].level) + "</div></div><div class=\"faction_xp\"><p class=\"xp_numbers numbers_" + localStorage.show_xp_numbers + "\">" + factions[data.id].progressToNextLevel + "/" + factions[data.id].nextLevelAt + "</p><div class=\"xp_bar\" style=\"width: " + next_level_percentage + "%;\"></div><div class=\"next_xp_bar\" style=\"width: " + remaining_experience + "%;\"></div></div><div class=\"faction_xp_extra_right\"><div class=\"level_number\">" + (factions[data.id].level + 1) + "</div><div class=\"extra_levels\">" + extra_levels + "</div></div></div></div></div>";
 									$("#prog_factions").html(factions_result);
 								}
 							});
@@ -696,26 +660,26 @@ function factionXP(){
 						 183980811: {"xp": 100,  "faction": 697030790},  //Crucible Token
 						3825769808: {"xp": 100,  "faction": 828982195},  //Io Token 
 						3756389242: {"xp": 50,   "faction": 828982195},  //Phaseglass Spire
-						1305274547: {"xp": 100,  "faction": 828982195},  //Phaseglass Needle
+						1305274547: {"xp": 250,  "faction": 828982195},  //Phaseglass Needle
 						 685157383: {"xp": 75,   "faction": 1021210278}, //Gunsmith Materials
 						 685157381: {"xp": 25,   "faction": 1021210278}, //Weapon Telemetry
 						3201839676: {"xp": 100,  "faction": 1660497607}, //Nessus Token
 						2949414982: {"xp": 50,   "faction": 1660497607}, //Quantized Datalattice
-						3487922223: {"xp": 100,  "faction": 1660497607}, //Microphasic Datalattice
+						3487922223: {"xp": 250,  "faction": 1660497607}, //Microphasic Datalattice
 						3957264072: {"xp": 1000, "faction": 3231773039}, //Vanguard Research Token
 						 494493680: {"xp": 100,  "faction": 4196149087}, //Archology Token
 						 461171930: {"xp": 50,   "faction": 4196149087}, //Alkane Spores
-						2014411539: {"xp": 100,  "faction": 4196149087}, //Alkane Dust
+						2014411539: {"xp": 250,  "faction": 4196149087}, //Alkane Dust
 						2640973641: {"xp": 100,  "faction": 4235119312}, //EDZ Token
 						 478751073: {"xp": 50,   "faction": 4235119312}, //Dusklight Crystal
-						 950899352: {"xp": 100,  "faction": 4235119312}, //Dusklight Shard
+						 950899352: {"xp": 250,  "faction": 4235119312}, //Dusklight Shard
 						1270564331: {"xp": 100,  "faction": 1714509342}, //FWC Token
 						2270228604: {"xp": 100,  "faction": 2105209711}, //New Monarchy Token
 						2959556799: {"xp": 100,  "faction": 3398051042}, //Dead Orbit Token
 						1873857625: {"xp": 100,  "faction": 1761642340}, //Iron Banner Token
 						3022799524: {"xp": 100,  "faction": 2677528157}, //Mercury Token
 						  49145143: {"xp": 50,   "faction": 2677528157}, //Simulation Seed
-						2386485406: {"xp": 100,  "faction": 2677528157}  //Simulation Bloom
+						2386485406: {"xp": 250,  "faction": 2677528157}  //Simulation Bloom
 					};
 
 					for(item in items){
